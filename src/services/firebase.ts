485fb941c1e53f77902eb9a6ee73324f1d9907dd
Tales Garcia <garcia.tales@gmail.com>
2021-06-27 20:51:28 -0300

    refactor(firebase): connect to auth and database

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,5 +1,8 @@
 import firebase from 'firebase/app';
 
+import 'firebase/auth';
+import 'firebase/database';
+
 var firebaseConfig = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
@@ -11,3 +14,6 @@ var firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const database = firebase.database();
